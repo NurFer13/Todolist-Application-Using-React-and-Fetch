11745444dd5235f9ec27d68e98519ded8d47926b
NurFer13 <nurfer1307@gmail.com>
2023-05-26 09:48:00 +0000

    started

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
 import React from "react";
-
+import { useState } from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      const newToDo = {
+        id: Math.random(),
+        todo: inputValue,
+      };
+      setToDoList(toDoList.concat(newToDo));
+      setInputValue("");
+    }
+  };
+  const handleDeleteToDoList = (id) => {
+    const newList = toDoList.filter((item) => id !== item.id);
+    setToDoList(newList);
+  };
   return (
-    <div className="text-center">
-      <h1 className="text-center mt-5">Hello Rigo!</h1>
-      <p>
-        <img src={rigoImage} />
-      </p>
-      <a href="#" className="btn btn-success">
-        Todolist Application Using React and Fetch
-      </a>
-      <p>
-        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-        love!
-      </p>
-    </div>
+    <>
+      <div className="card  text-center">
+        <div className="header">
+          <h1>To do List</h1>
+        </div>
+        <ul>
+          <li>
+            <input
+              type="text"
+              placeholder="What needs to be done"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+          </li>
+          {toDoList.map((item) => (
+            <li key={item.id}>
+              {item.toDoList}
+
+              <i
+                className=" icono fas fa-trash"
+                onClick={() => handleDeleteToDoList(item.id)}
+              >
+                {" "}
+              </i>
+            </li>
+          ))}
+        </ul>
+
+        <div className="footer">{toDoList.length} Tasks</div>
+      </div>
+    </>
   );
 };
 
