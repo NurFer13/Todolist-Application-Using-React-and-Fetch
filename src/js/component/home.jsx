a9059274caf7aaf4f3d9ee31bd626d2812f0cc89
NurFer13 <nurfer1307@gmail.com>
2023-05-29 11:40:47 +0000

    fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,27 @@ import { useState } from "react";
 
 //create your first component
 const Home = () => {
+  fetch("https://assets.breatheco.de/apis/fake/todos/user/nuria", {
+    method: "POST",
+    body: JSON.stringify(todos),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => {
+      console.log(resp.ok); // will be true if the response is successfull
+      console.log(resp.status); // the status code = 200 or code = 400 etc.
+      console.log(resp.text()); // will try return the exact result as string
+      return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then((data) => {
+      //here is where your code should start after the fetch finishes
+      console.log(data); //this will print on the console the exact object received from the server
+    })
+    .catch((error) => {
+      //error handling
+      console.log(error);
+    });
   const [inputValue, setInputValue] = useState("");
   const [toDoList, setToDoList] = useState([]);
   const handleKeyDown = (e) => {
